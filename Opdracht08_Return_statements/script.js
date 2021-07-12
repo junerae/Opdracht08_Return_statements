1362163d251e948c3cbafa7d9c729f36556a6ab0
junerae <80211043+junerae@users.noreply.github.com>
2021-07-12 14:13:05 +0200

    Add files via upload

---
+++ Opdracht08_Return_statements/script.js
@@ -0,0 +1,44 @@
+
+// Checking if a number is big
+
+// const uitrekenen = function (number1, number2) {
+//     const sum = number1 + number2;
+//     if (sum > 100) {
+//         return "true";
+//     } else (sum < 100) 
+//         return "false";
+// };
+
+// const result = uitrekenen(100, 5);
+// console.log (result);
+
+
+
+
+// Bouncer at a club
+
+// const clubAcces = function (age, peopleInside) {
+//     const sum = age && peopleInside;
+//     if (age > 18, peopleInside < 100) {
+//         return "Come in";
+//     } else if (age < 18) {
+//         return "This is a club for adults"
+//     } else if (peopleInside > 100) {
+//         return "It's too busy now, come back later"
+//     }
+// }
+
+// const result = clubAcces(20, 101);
+// console.log (result)
+
+
+
+// Calculating the average
+
+const averageNumber = function (numbers) {
+    const sum = (numbers / averageNumber.length);
+    return sum;
+}
+
+const result = averageNumber([1, 1, 1, 1, 1]);
+console.log(result);
